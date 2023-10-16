36a9c38752e1f85dddd5738bc282cf946fa1d895
Ying Quan <quanying26@hotmail.com>
2023-10-16 01:07:45 -0400

    add Paper component

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Box, Stack, Divider, Grid,} from "@mui/material";
+import { Box, Stack, Divider, Grid, Paper} from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Paper sx={{padding: "32px"}} elevation={4}>
       <Stack
-        sx={{ border: "ipx solid" }}
+        sx={{ border: "1px solid" }}
         direction="row"
         spacing={2}
         divider={<Divider orientation="vertical" flexItem />}
@@ -56,6 +56,6 @@ export const MuiLayout = () => {
           </Box>
         </Grid>
       </Grid>
-    </>
+    </Paper>
   );
 };
