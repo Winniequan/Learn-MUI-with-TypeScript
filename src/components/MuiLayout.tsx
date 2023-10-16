637490f4aab8654085e30a6f89726932c45c5611
Ying Quan <quanying26@hotmail.com>
2023-10-16 00:20:52 -0400

    add Stack one-dimension layout  Divider component

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Divider} from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Stack sx={{border: "ipx solid" }} direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem/>}>
       <Box
         component="span"
         sx={{
@@ -20,6 +20,6 @@ export const MuiLayout = () => {
         Codevolution
       </Box>
       <Box display="flex" height="100px" width="100px" bgcolor="success.light" p={2}></Box>
-    </>
+    </Stack>
   );
 };
